5e79b06e170a56f0781c5e2bb011b269f23c9dc8
herlandro <herlandro.hermogenes@gmail.com>
2018-04-09 20:23:22 +0100

    Added extension CharacterSet

---
+++ UIKitPrime/Classes/UIKitPrime/extension/CharacterSet.swift
@@ -0,0 +1,15 @@
+//
+//  UIView.swift
+//  UIKitPrime
+//
+//  Created by Herlandro Hermogenes on 09/04/2018.
+//
+
+import Foundation
+
+public extension CharacterSet {
+
+    static var numerics: CharacterSet {
+        return CharacterSet(charactersIn: "1234567890")
+    }
+}
