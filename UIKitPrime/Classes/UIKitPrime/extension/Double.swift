7085aa79056bbdc9354f8e4b205059d2c1bce2ec
herlandro <herlandro.hermogenes@gmail.com>
2018-04-12 23:35:16 +0100

    Updated extension Double

--- UIKitPrime/Classes/UIKitPrime/extension/Double.swift
+++ UIKitPrime/Classes/UIKitPrime/extension/Double.swift
@@ -9,5 +9,8 @@ import Foundation
 
 public extension Double {
 
-	
+	func roundDecimalPlaces(toPlaces places: Int) -> Double {
+		let divisor = pow(10.0, Double(places))
+		return (self * divisor).rounded() / divisor
+	}
 }
