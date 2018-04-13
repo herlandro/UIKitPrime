22f487eda1d2ba3b2223a893171f1b2e1e2c11ab
herlandro <herlandro.hermogenes@gmail.com>
2018-04-13 20:35:16 +0100

    Updated extension LAContext

--- UIKitPrime/Classes/UIKitPrime/extension/LAContext.swift
+++ UIKitPrime/Classes/UIKitPrime/extension/LAContext.swift
@@ -16,4 +16,33 @@ public extension LAContext {
 		case touchID
 		case faceID
 	}
+
+	var biometricType: BiometricTypeEnum {
+		var error: NSError?
+
+		guard self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+			// Capture these recoverable error thru Crashlytics
+			return .none
+		}
+
+		if #available(iOS 11.0, *) {
+
+			switch self.biometryType {
+			case .none:
+				return .none
+
+			case .touchID:
+				return .touchID
+
+			case .faceID:
+				return .faceID
+
+			default:
+				return .none
+			}
+		} else {
+
+			return self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touchID : .none
+		}
+	}
 }
