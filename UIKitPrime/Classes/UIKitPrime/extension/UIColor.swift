8325336a9d53b7c367b3091d58ed0b9ab1f5a510
herlandro <herlandro.hermogenes@gmail.com>
2018-04-23 14:47:00 +0100

    Update extension UIColor

--- UIKitPrime/Classes/UIKitPrime/extension/UIColor.swift
+++ UIKitPrime/Classes/UIKitPrime/extension/UIColor.swift
@@ -9,5 +9,13 @@ import UIKit
 
 public extension UIColor {
 
-
+	var hexString: String {
+		var r: CGFloat = 0
+		var g: CGFloat = 0
+		var b: CGFloat = 0
+		var a: CGFloat = 0
+		getRed(&r, green: &g, blue: &b, alpha: &a)
+		let rgb: Int32 = (Int32)(r * 255) << 16 | (Int32)(g * 255) << 8 | (Int32)(b * 255) << 0
+		return String(format: "#%06x", rgb)
+	}
 }
