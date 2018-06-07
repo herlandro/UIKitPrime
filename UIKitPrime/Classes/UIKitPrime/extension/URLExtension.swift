e409d2fdb29725b111886a86de25b73849b0ee68
herlandro <herlandro.hermogenes@gmail.com>
2018-06-07 20:59:42 +0100

    Update extension URL

--- UIKitPrime/Classes/UIKitPrime/extension/URLExtension.swift
+++ UIKitPrime/Classes/UIKitPrime/extension/URLExtension.swift
@@ -1,9 +1,8 @@
 //
-//  URLExtension.swift
-//  Best Mobile Prod
+//  Character.swift
+//  UIKitPrime
 //
-//  Created by Joao Pires on 04/07/2019.
-//  Copyright © 2019 Joao Pires. All rights reserved.
+//  Created by Herlandro Hermogenes on 07/05/2018.
 //
 
 import Foundation
@@ -11,13 +10,10 @@ import Foundation
 public extension URL {
 
     static func encode(string: String) -> String {
-
         let allowedCharacterSet = (CharacterSet(charactersIn: "^!*'();:@&=+$,/?%#[] ").inverted)
         if let escapedString = string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) {
-
             return escapedString
         }
-
         return string
     }
 }
