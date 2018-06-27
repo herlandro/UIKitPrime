10951a59421749a9f9b2079893e95c954f7d27b7
herlandro <herlandro.hermogenes@gmail.com>
2018-06-27 21:08:04 +0100

    Added struct AppHelper

---
+++ UIKitPrime/Classes/UIKitPrime/helper/AppHelper.swift
@@ -0,0 +1,28 @@
+//
+//  Logger.swift
+//  UIKitPrime
+//
+//  Created by Herlandro Hermogenes on 27/06/2018.
+//
+
+import Foundation
+
+public struct AppHelper {
+
+
+	static public var appName: String {
+		return Bundle.main.infoDictionary!["CFBundleName"] as? String ?? ""
+	}
+
+	static public var appVersion: String {
+		return Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String ?? ""
+	}
+
+	static public var appIdentifier: String {
+		return Bundle.main.bundleIdentifier ?? ""
+	}
+
+	static public var appBuild: String {
+		return Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
+	}
+}
