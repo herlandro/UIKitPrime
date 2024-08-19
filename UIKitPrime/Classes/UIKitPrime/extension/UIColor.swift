9e6f98661316395e4c9daef53c82f995db3a2cc7
herlandro <herlandro.hermogenes@gmail.com>
2024-08-19 18:25:40 +0100

    Refactored UIColor extension hexa init

--- UIKitPrime/Classes/UIKitPrime/extension/UIColor.swift
+++ UIKitPrime/Classes/UIKitPrime/extension/UIColor.swift
@@ -21,14 +21,14 @@ public extension UIColor {
 
     convenience init(hex: String) {
         var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        if (cString.hasPrefix("#")) {
+        if cString.hasPrefix("#") {
             cString.remove(at: cString.startIndex)
         }
-        if ((cString.count) != 6) {
+        if cString.count != 6 {
             preconditionFailure("The hex string size is != 6")
         }
-        var rgbValue: UInt32 = 0
-        Scanner(string: cString).scanHexInt32(&rgbValue)
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
         self.init(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
